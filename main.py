d9216c49510028bd3860769d3bb64b5d3ad45cae
Carlos Puente <carlospuenteg@gmail.com>
2022-06-05 15:55:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import numpy as np
 from PIL import Image
 
 NORMAL = ("■","▤","◧","◫","□")
-EXTENDED = ("■■","■▤","▤▤","▤◧","◧◧","◧◫","◫◫","◫□","□□","□-","--","- ","  ")
+EXTENDED = ("☗☗","☗■","■■","■▞","▞▞","▞➎","➎➎","➎●","●●","●▒","▒▒","▒◆","◆◆","◆▤","▤▤","▤░","░░","░◧","◧◧","◧◫","◫◫","◫☑","☑☑","☑▮","▮▮","▮▯","▯▯","▯□","□□","□-","□✚","✚✚","✚+","++","+-","--","-.","..",". ","  ")
+
 def img_ascii(file, new_file="ascii_image.txt", ASCII=EXTENDED):
     res = ""
     img = np.array(Image.open("images/" + file))
     for line in img:
         for pixel in line:
             for i in range(len(ASCII)):
-                if min(pixel) < 255/(len(ASCII)-i):
+                if np.average(pixel) <= 255/(len(ASCII)-i):
                     res += ASCII[i]
                     break
         res += "\n"
     
     open("results/" + new_file, "w").write(res)
 
-img_ascii("rabbit-small.jpeg")
+img_ascii("rabbit-contrast.jpeg", "rabbit-contrast.txt")
