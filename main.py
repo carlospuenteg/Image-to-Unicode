31603cae3cda8b19c6859727ebb49e96b7ea7487
Carlos Puente <carlospuenteg@gmail.com>
2022-06-05 15:56:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import numpy as np
 from PIL import Image
 
-UNI = "_,.ˉ˙ "
+UNI_1 = "*<:,.ˉ˙ "
+UNI = "@#ma*<;:,.·˙ "
 EXTENDED = [c * 2 for c in UNI]
 
 def img_ascii(file, new_file="ascii_image.txt", ASCII=EXTENDED):
@@ -9,9 +10,10 @@ def img_ascii(file, new_file="ascii_image.txt", ASCII=EXTENDED):
     img = np.array(Image.open("images/" + file))
     for line in img:
         for pixel in line:
-            res += ASCII[int(sum(pixel)/768*4)]
+            res += ASCII[int(sum(pixel)/768*len(UNI))]
         res += "\n"
     
     open("results/" + new_file, "w").write(res)
 
+#img_ascii("gradient-h.jpeg", "gradient.txt")
 img_ascii("rabbit.jpeg", "rabbit.txt")
